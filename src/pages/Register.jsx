4e4b3493748e2f72bc8daa0a4f52a6055e5254af
Haoyu Cheng <chengyu2000311@gmail.com>
2021-01-20 11:03:59 +0800

    add register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Idm from "../services/Idm";
 import "../css/form.css";
 
 const Register = () => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    /**
+     * @param e Event
+     */
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        Idm.register(email, password)
+            .then(response => alert(JSON.stringify(response.data, null, 4)))
+            .catch(error => alert(error));
+    }
+
     return (
       <div className="form-box">
         <h1>Register</h1>
+        <form onSubmit={handleRegister}>
+          <label className="form-label">Email</label>
+          <input
+            className="form-input"
+            type="email"
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+          <label className="form-label">Password</label>
+            <input
+              className="form-input"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          <button className="form-button">Register</button>
+        </form>
       </div>
     );
 }
--- src/services/Idm.js
+++ src/services/Idm.js
@@ -16,6 +16,21 @@ async function login(email, password) {
     return await Socket.POST(options);
 }
 
+async function register(email, password) {
+    const payLoad = {
+        email: email,
+        password: password.split("")
+    };
+
+    const options = {
+        baseURL: idmUrl, // Base URL
+        url: idmEPs.registerEP, // Path of URL
+        data: payLoad // Data to send in Body
+    }
+
+    return await Socket.POST(options);
+}
+
 export default {
-    login
+    login, register
 };
